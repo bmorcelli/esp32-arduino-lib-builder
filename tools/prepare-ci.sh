d6a9dbb5da92101c963069cd3865659d36b8d85b
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-10-15 10:54:50 -0300

    Update prepare-ci.sh

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 sudo apt update && sudo apt install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py
-sudo apt install -y python3-pip python3-setuptools python3-venv python3-serial python3-click python3-future python3-wheel python3-cryptography python3-pyparsing python3-pyelftools
-
-#pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
+pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools

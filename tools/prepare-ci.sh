ce750b262d05d26ac35630add033f27445acd66c
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-10-15 10:22:16 -0300

    Update prepare-ci.sh

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
-python3-pip3 && \
+sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache python3-pip
 pip3 install setuptools pyserial click future wheel cryptography pyparsing pyelftools

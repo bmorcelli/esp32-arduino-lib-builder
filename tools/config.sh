0e348c297e2c6c3662459edbe63c41ceb84d8d47
Pirata <104320209+bmorcelli@users.noreply.github.com>
2026-05-19 00:51:54 +0000

    launcher

--- tools/config.sh
+++ tools/config.sh
@@ -6,11 +6,11 @@ if [ -z $IDF_PATH ]; then
 fi
 
 if [ -z $IDF_BRANCH ]; then
-    IDF_BRANCH="release/v5.5"
+    IDF_BRANCH="release/v5.5launcher2"
 fi
 
 if [ -z $AR_PR_TARGET_BRANCH ]; then
-    AR_PR_TARGET_BRANCH="master"
+    AR_PR_TARGET_BRANCH="release/v5.5launcher2"
 fi
 
 if [ -z $IDF_TARGET ]; then

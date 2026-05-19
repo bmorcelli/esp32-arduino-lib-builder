4bff5d6f7c9a452a5314d78e16aaefb0af2f0f54
Pirata <bmorcelli@gmail.com>
2026-05-18 22:27:09 -0300

    ...

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -8,10 +8,13 @@ source ./tools/config.sh
 echo "Updating TinyUSB..."
 TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
 TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
-if [ ! -d "$TINYUSB_REPO_DIR" ]; then
-    git clone "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
+TINYUSB_BRANCH="${TINYUSB_BRANCH:-master}"
+TINYUSB_COMMIT="${TINYUSB_COMMIT:-d9ad09a8d}"
+if [ ! -d "$TINYUSB_REPO_DIR/.git" ]; then
+    rm -rf "$TINYUSB_REPO_DIR"
+    git clone --branch "$TINYUSB_BRANCH" "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
 else
-    git -C "$TINYUSB_REPO_DIR" fetch && \
-    git -C "$TINYUSB_REPO_DIR" pull --ff-only
+    git -C "$TINYUSB_REPO_DIR" fetch origin "$TINYUSB_BRANCH"
 fi
+git -C "$TINYUSB_REPO_DIR" checkout "$TINYUSB_COMMIT"
 if [ $? -ne 0 ]; then exit 1; fi
